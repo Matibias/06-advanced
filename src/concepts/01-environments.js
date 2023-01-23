b983a64a5a88a3427d3d9a76c488d549848618da
Matias Parra <mparra@retailsbs.com>
2023-01-22 22:34:20 -0300

    first commit

---
+++ src/concepts/01-environments.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const EnvironmentsComponent = ( element ) => {
+
+  const html = `
+    variables:
+  `;
+  element.innerHTML = html;
+
+}
