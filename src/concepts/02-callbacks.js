0b00745b2f0dec842ec60e5f2da8c78e104a9d6d
Matias Parra <mparra@retailsbs.com>
2023-01-23 00:00:10 -0300

    Callbacks Hells

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -7,8 +7,9 @@ import { heroes } from "../data/heroes";
 
 export const callbacksComponent = (element) => {
   console.log("callbacksComponent");
-  const id = "5d86371f25a058e5b1c8a65e";
-  findHero(id , (error, hero) => {
+  const id1 = "5d86371f25a058e5b1c8a65e";
+  const id2 = "5d86371f2343e37870b91ef1"
+  findHero(id1 , (error, hero1) => {
     // element.innerHTML = hero?.name || 'No hay heroe'
 
     if (error) {
@@ -16,7 +17,16 @@ export const callbacksComponent = (element) => {
       return;
     }
 
-    element.innerHTML = hero.name
+    findHero(id2 , (error, hero2) => {
+      // element.innerHTML = hero?.name || 'No hay heroe'
+  
+      if (error) {
+        element.innerHTML = error;
+        return;
+      }
+  
+      element.innerHTML = `${hero1.name} / ${hero2.name}`
+    });
   });
 };
 
